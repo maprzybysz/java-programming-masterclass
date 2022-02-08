77a89edbded1f295d212413982aaa196d98fa02d
mprzybysz <notentego678@gmail.com>
2022-02-08 20:56:22 +0100

    03.starting-out-with-expressions

--- src/Hello.java
+++ src/Hello.java
@@ -3,9 +3,14 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
 
-        int myFirstNumber = 5 + 15;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
 
-        System.out.println(myFirstNumber);
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
 
     }
 }

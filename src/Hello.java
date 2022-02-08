dafa7262febb99bdbfe2ebe897a939b2400e41d4
mprzybysz <notentego678@gmail.com>
2022-02-08 20:47:36 +0100

    03.variables

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,10 @@ public class Hello {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        int myFirstNumber = 5 + 15;
+
+        System.out.println(myFirstNumber);
+
     }
 }

902214351c226b37a325ae300caf75402e94f567
mprzybysz <notentego678@gmail.com>
2022-04-25 17:45:55 +0200

    03.byte, short, long and width

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -8,7 +8,23 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer min Value = " + myMinIntValue);
         System.out.println("Integer max Value = " + myMaxIntValue);
-        System.out.println("Busted MAX value = " + (myMaxIntValue +1));
-        System.out.println("Busted MAX value = " + (myMinIntValue -1));
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MAX value = " + (myMinIntValue - 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte min Value = " + myMinByteValue);
+        System.out.println("Byte min Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short min Value = " + myMinShortValue);
+        System.out.println("Short max Value = " +myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long  myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long min Value = " + myMinLongValue);
+        System.out.println("Long max Value = " +myMaxLongValue);
     }
 }

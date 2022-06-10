c2f4bf7424b50f86506dfb8e9b61f630921fecee
mprzybysz <mprzybysz.dev@gmail.com>
2022-06-10 20:09:42 +0200

    03.Primitive types challenge

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -7,33 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myMinIntValue = Integer.MIN_VALUE;
-        int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer min Value = " + myMinIntValue);
-        System.out.println("Integer max Value = " + myMaxIntValue);
-        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
-        System.out.println("Busted MAX value = " + (myMinIntValue - 1));
-
-        byte myMinByteValue = Byte.MIN_VALUE;
-        byte myMaxByteValue = Byte.MAX_VALUE;
-        System.out.println("Byte min Value = " + myMinByteValue);
-        System.out.println("Byte min Value = " + myMaxByteValue);
-
-        short myMinShortValue = Short.MIN_VALUE;
-        short myMaxShortValue = Short.MAX_VALUE;
-        System.out.println("Short min Value = " + myMinShortValue);
-        System.out.println("Short max Value = " +myMaxShortValue);
-
-        long myLongValue = 100L;
-        long myMinLongValue = Long.MIN_VALUE;
-        long  myMaxLongValue = Long.MAX_VALUE;
-        System.out.println("Long min Value = " + myMinLongValue);
-        System.out.println("Long max Value = " +myMaxLongValue);
-
-        int myTotal = (myMinIntValue /2);
-        byte myNewByteValue = (byte)(myMinByteValue/2);
-
-        short myNewShortValue = (short) (myMinShortValue/ 2);
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
 
     }
 
